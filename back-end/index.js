183fa6a3f2ac653f9e01c7b9b31dee3ba90c0ef0
Pawel Armatys <piwirm@gmail.com>
2021-11-24 20:30:33 +0100

    CORS fix

--- back-end/index.js
+++ back-end/index.js
@@ -8,10 +8,7 @@ const port = process.env.PORT || 3000;
 const cmc = require('./cmc');
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+
 // Use if you're behind a reverse proxy
 // https://expressjs.com/en/guide/behind-proxies.html
 // app.set('trust proxy', 1);
@@ -31,15 +28,15 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // If needed: Allow options to pass CORS preflight check
-/* app.options("/*", (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+app.options('/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type,Authorization,Content-Length,X-Requested-With"
+    'Access-Control-Allow-Headers',
+    'Content-Type,Authorization,Content-Length,X-Requested-With'
   );
   res.sendStatus(200);
-}); */
+});
 
 const limiter = rateLimit({
   windowMs: 1000,
--- src/endpoints/coin-market-cap/cryptocurrency-map.ts
+++ src/endpoints/coin-market-cap/cryptocurrency-map.ts
@@ -2,18 +2,14 @@ import axios from 'axios';
 import { CoinMarketCapResponse } from 'types/coin-market-cap';
 
 export const getCMCCurrenciesMapDataFromAPI = async () => {
-  try {
-    return await axios.get<CoinMarketCapResponse>(
-      'https://coin-slate.herokuapp.com/cmc/cryptocurrency/map'
-    );
-  } catch (error) {
-    console.error(error);
-  }
+  return await axios.get<CoinMarketCapResponse>(
+    'https://coin-slate.herokuapp.com/cmc/cryptocurrency/map'
+  );
 };
 
 export const getCMCCurrenciesMap = async () => {
   const result = await getCMCCurrenciesMapDataFromAPI();
-  if (result?.data.status.error_code === 0) {
+  if (result.data.status.error_code === 0) {
     return result.data.data;
   }
 };
